d3462ab4cbcfc79965fd4ec68ad2e5c753a5f3a9
nothingtosay920 <963717822@qq.com>
2022-02-19 21:37:28 +0800

    gather

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -2,12 +2,47 @@ import { Button, Input, List } from '@arco-design/web-react';
 import Editor from 'md-editor-rt';
 import { useState } from 'react';
 import style from '../styles/Writing.module.sass'
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
+
+
+type data = {
+  id: number,
+  content: string
+} []
+
+const reorder = (list: data, startIndex: number, endIndex: number): data => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getListStyle = (isDraggingOver:boolean) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: "0",
+  width: 150
+})
+
+const getItemStyle = (isDragging: any, draggableStyle: any) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  margin: `0 0 15px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
 
 export default function UserArticle() {
-  const [text, setText] = useState('hello md-editor-rt');
-  const [data, setdata] = useState(['1', '2', '3'])
+  const [data, setdata] = useState([{id: 0, content: '1'}, {id: 1, content: '2'}, {id: 2, content: '3'}])
   const [foucs, setfoucs] = useState(0)
+
+  const onDragend = (result: DropResult) => {
+    if(!result.destination) return
+    const newdata = reorder(data, result.source.index, result.destination.index)
+    setdata(newdata)
+  }
   return (
     <div className={style.wrapper}>
       <header className={style.header}>
@@ -24,8 +59,9 @@ export default function UserArticle() {
         </div>
       </header>
       <div className={style.writing}>
-        <div className={style.catalogue}>
-        <List
+        <div className={style.catalogue} onClick={(e) => {
+        }}>
+        {/* <List
           className={style.list}
           size='small'
           header='List title'
@@ -36,12 +72,44 @@ export default function UserArticle() {
                 {index+1}
               </Button>
             </List.Item>}
-        />
+        /> */}
+        <DragDropContext onDragEnd={onDragend}>
+          <Droppable droppableId="droppable">
+            {
+              (provided, snapshot) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)} 
+                  >
+                    {data.map((item, index) => (
+                      <Draggable key={item.id} draggableId={String(item.id)} index={index}>
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}
+                          >
+                              <div onClick={() => {setfoucs(index)}}>{item.id+1}</div>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}  
+                </div>
+              )
+            }
+          </Droppable>
+        </DragDropContext>
         </div>
         <Editor 
-          modelValue={data[foucs]}
+          modelValue={data[foucs].content}
           onChange={(text) => setdata((state) => {
-            state[foucs] = text
+            state[foucs].content = text
             return state
           })}
           style={{height: 550, minHeight: 200}} />
